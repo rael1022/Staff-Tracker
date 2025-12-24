b52e9ae326e404dc59ea2a23085a80012574f026
Kong Wen Khang <w.kong70@yahoo.com>
2025-12-24 09:24:29 +0800

    fix register position bug

--- backend/StaffTracker/accounts/views.py
+++ backend/StaffTracker/accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import UserProfile
-
+from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .models import UserProfile
@@ -16,7 +16,7 @@ def login_view(request):
 
         if user is not None:
             try:
-                profile = user.userprofile  # Get the UserProfile
+                profile = user.userprofile  
                 if not profile.is_approved:
                     return render(request, 'login/login.html', {'error': 'You must be approved by an admin before logging in.'})
             except UserProfile.DoesNotExist:
@@ -28,7 +28,6 @@ def login_view(request):
             return render(request, 'login/login.html', {'error': 'Invalid username or password.'})
     
     return render(request, 'login/login.html')
-
 
 def register_view(request):
     if request.method == 'POST':
@@ -48,28 +47,45 @@ def register_view(request):
     
     return render(request, 'register/register.html')
 
-
-@login_required
-
 @login_required
 def dashboard(request):
     user = request.user
+
     if user.groups.filter(name='HR').exists():
+
         if request.method == 'POST':
             approve_id = request.POST.get('approve_id')
+
             try:
                 profile = UserProfile.objects.get(user__id=approve_id)
+
                 profile.is_approved = True
                 profile.save()
+
+                role = profile.role  
+
+                group, _ = Group.objects.get_or_create(name=role)
+
+                approved_user = profile.user
+                approved_user.groups.clear()
+                approved_user.groups.add(group)
+
             except UserProfile.DoesNotExist:
                 pass
 
         pending_users = UserProfile.objects.filter(is_approved=False)
-        return render(request, 'dashboard/hr_dashboard.html', {'pending_users': pending_users})
+        return render(
+            request,
+            'dashboard/hr_dashboard.html',
+            {'pending_users': pending_users}
+        )
 
     elif user.groups.filter(name='HOD').exists():
         return render(request, 'dashboard/hod_dashboard.html')
+
     elif user.groups.filter(name='Trainer').exists():
         return render(request, 'dashboard/trainer_dashboard.html')
+
     else:
         return render(request, 'dashboard/employee_dashboard.html')
+   

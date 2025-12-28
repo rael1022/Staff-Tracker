a812abbc9fdbcfe3ff70715f54f7cf489c272b96
Kong Wen Khang <w.kong70@yahoo.com>
2025-12-28 12:56:02 +0800

    hr manage acc

--- backend/StaffTracker/accounts/urls.py
+++ backend/StaffTracker/accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('hr/create-user/', views.hr_create_user, name='hr_create_user'),
+    path('hr/update-user/<int:user_id>/', views.hr_update_user, name='hr_update_user'),
+    path('hr/toggle-user/<int:user_id>/', views.hr_toggle_user, name='hr_toggle_user'),
 ]

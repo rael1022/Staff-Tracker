5a5e2d8e97d4147ec9c84149458ebac0e3eec2f5
Kong Wen Khang <w.kong70@yahoo.com>
2025-12-30 16:31:20 +0800

    combine with certificate

--- backend/StaffTracker/certificate/urls.py
+++ backend/StaffTracker/certificate/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import hr_delete_certificate
 
 urlpatterns = [
     path('my-certificates/', views.certificate_list, name='my_certificates'),
     path('issued/', views.issued_certificates, name='issued_certificates'),
     path('preview/<int:pk>/', views.certificate_preview, name='certificate_preview'),
+
+    path('hr/delete/<int:cert_id>/', hr_delete_certificate, name='hr_delete_certificate'),
 ]

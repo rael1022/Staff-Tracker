f760aa4f825f609b4cfb7cea4769fd96aa8c977b
Kong Wen Khang <w.kong70@yahoo.com>
2026-01-05 17:38:37 +0800

    debug

--- backend/StaffTracker/department/apps.py
+++ backend/StaffTracker/department/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class DepartmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'department'
+
+    def ready(self):
+        from .models import Department
+
+        def create_default_departments(sender, **kwargs):
+            dept_names = ['IT', 'Finance', 'Marketing', 'Training', 'Ops', 'HR Ops']
+            for name in dept_names:
+                Department.objects.get_or_create(name=name)
+
+        post_migrate.connect(create_default_departments, sender=self)

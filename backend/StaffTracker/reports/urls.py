3e10250e4e78298a1a87e604561ff4aefb85574c
rael1022 <lawys1022@gmail.com>
2026-01-09 21:46:15 +0800

    add hr view cpd summary report

--- backend/StaffTracker/reports/urls.py
+++ backend/StaffTracker/reports/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.reports_dashboard, name='reports_dashboard'),
     path('hr/certificates/', views.hr_certificate_report,name='hr_certificate_report'),
     path('cpd-summary/', views.cpd_summary_report, name='cpd_summary'),
+    path('cpd-summary/download/', views.cpd_summary_download, name='cpd_summary_download'),
     path('attendance-summary/', views.attendance_summary_report, name='attendance_summary'),
     path('hod-department-training-progress/', views.hod_department_training_progress, name='hod_department_training_progress'),
     path('hod/department-report/', views.hod_department_report, name='hod_department_report'),

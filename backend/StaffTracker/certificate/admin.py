295fcf15a2b973945ae00182ddad139fef307f15
KumBS <kumbs0329@gmail.com>
2026-01-10 19:34:02 +0800

    admin.py replenish

--- backend/StaffTracker/certificate/admin.py
+++ backend/StaffTracker/certificate/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Certificate
 
-# Register your models here.
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'training',
+        'trainer',
+        'issue_date',
+        'expiry_date',
+        'is_expired',
+        'reminder_soon_sent',
+        'reminder_expired_sent',
+    )
+    list_filter = (
+        'expiry_date',
+        'trainer',
+        'reminder_soon_sent',
+        'reminder_expired_sent',
+    )
+    search_fields = (
+        'user__username',
+        'training__title',
+    )
+    ordering = ('expiry_date',)
+    readonly_fields = ('issue_date',)
+    fieldsets = (
+        ('Certificate Info', {
+            'fields': ('user', 'training', 'trainer')
+        }),
+        ('Dates', {
+            'fields': ('issue_date', 'expiry_date')
+        }),
+        ('Reminder Status', {
+            'fields': ('reminder_soon_sent', 'reminder_expired_sent')
+        }),
+    )
